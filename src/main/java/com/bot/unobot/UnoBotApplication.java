af465304e7191763571e602419bbc45c1fd490e0
Gagah Pangeran <gagahpangeran@gmail.com>
2019-04-02 17:39:20 +0700

    create method for reply message

--- src/main/java/com/bot/unobot/UnoBotApplication.java
+++ src/main/java/com/bot/unobot/UnoBotApplication.java
@@ -1,17 +1,27 @@
 package com.bot.unobot;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.xml.soap.Text;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 @LineMessageHandler
 public class UnoBotApplication {
+
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
     public static void main(String[] args) {
         SpringApplication.run(UnoBotApplication.class, args);
     }
@@ -20,10 +30,11 @@ public class UnoBotApplication {
     public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
         String msg = event.getMessage().getText();
+        String replyToken = event.getReplyToken();
 
         // Placeholder
         if (msg.charAt(0) == '!') {
-            replyMessage(msg.substring(1));
+            replyMessage(replyToken, msg.substring(1));
         }
     }
 
@@ -32,7 +43,12 @@ public class UnoBotApplication {
         System.out.println("event: " + event);
     }
 
-    public TextMessage replyMessage(String msg) {
-        return new TextMessage(msg);
+    public void replyMessage(String token, String msg) {
+        TextMessage reply = new TextMessage(msg);
+        try {
+            lineMessagingClient.replyMessage(new ReplyMessage(token, reply)).get();
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("There's something wrong");
+        }
     }
 }
