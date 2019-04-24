5cd09be6e56f653d742b2c2da807836c43c356b1
Jeffrey <jeffreywong0812@gmail.com>
2019-04-24 13:57:24 +0700

    Add Unit Tests to Card class

---
+++ src/test/java/com/bot/unobot/card/WildCardTest.java
@@ -0,0 +1,25 @@
+package com.bot.unobot.card;
+
+import com.bot.unobot.card.WildCard;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class WildCardTest {
+
+    public WildCard card;
+
+    @Test
+    public void OrdinaryCards_Test(){
+        card = new WildCard("yellow");
+        Assert.assertThat(card.getColor(), CoreMatchers.is("yellow"));
+        Assert.assertThat(card.getName(), CoreMatchers.is("Wildcard"));
+    }
+}
