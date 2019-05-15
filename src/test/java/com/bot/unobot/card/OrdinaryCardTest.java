6c1f61425042b41f6090443da30d9a0ea6623f77
Gagah Pangeran <gagahpangeran@gmail.com>
2019-05-15 13:45:07 +0700

    fix conflict

--- src/test/java/com/bot/unobot/card/OrdinaryCardTest.java
+++ src/test/java/com/bot/unobot/card/OrdinaryCardTest.java
@@ -2,6 +2,7 @@ package com.bot.unobot.card;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,19 +13,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 public class OrdinaryCardTest {
 
-    @Test
-<<<<<<< HEAD
-    public void ordinaryCardsTest(){
-=======
+    public OrdinaryCard card;
 
+    @Test
     public void OrdinaryCards_Test(){
->>>>>>> 5c03f0a61e934e8619dc350671d789cf8ad1284e
         OrdinaryCard testCard = new OrdinaryCard("7",Color.GREEN);
         Assert.assertThat(testCard.getEffect(), CoreMatchers.is(Effect.NOTHING));
         Assert.assertEquals(Effect.NOTHING, testCard.getEffect());
         testCard.setColor(Color.BLUE);
         Assert.assertThat(testCard.getColor(), CoreMatchers.is(Color.BLUE));
-        testCard.setSymbol("8");
-        Assert.assertThat(testCard.getSymbol(), CoreMatchers.is("1"));
     }
 }
+
